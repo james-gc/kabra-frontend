32ef3404545010e1e4831d9a5fd5818c2e228f2d
James Christian <jameschristian.totalo@gmail.com>
2020-02-12 16:28:39 +0530

    added template

--- src/pages/products.public.jsx
+++ src/pages/products.public.jsx
@@ -1,22 +1,56 @@
 import React from "react";
 import { Row, Col } from "@bootstrap-styled/v4";
+import _map from "lodash/map";
+
 import ProductItem from "../components/ProductItem";
+
+const products = [
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  },
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  },
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  },
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  },
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  },
+  {
+    name: "ABC",
+    description: "abc description",
+    image: "https://laksdjf.jpg",
+    price: 200.32
+  }
+];
 
 export default function ProductPublicList() {
   return (
     <Row>
-      <Col lg={6} md={2}>
-        <ProductItem />
-      </Col>
-      <Col lg={6} md={2}>
-        <ProductItem />
-      </Col>
-      <Col lg={6} md={2}>
-        <ProductItem />
-      </Col>
-      <Col lg={6} md={2}>
-        <ProductItem />
-      </Col>
+      {_map(products, (product,i) => (
+        <Col key={i} lg={6} md={6}>
+          <ProductItem {...product} />
+        </Col>
+      ))}
     </Row>
   );
 }
